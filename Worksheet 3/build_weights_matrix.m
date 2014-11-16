b554dc8ece16c627309b8c5839ba15d8ef083e5e
JohRausch <01joh.rausch@gmail.com>
2014-11-16 23:22:39 +0100

    Restored build_weights_matrix

---
+++ Worksheet 3/build_weights_matrix.m
@@ -0,0 +1,48 @@
+function [ weights_matrix,storage ] = build_weights_matrix( N_x,N_y )
+%build_weights_matrix Assembles the discrete Laplacian Operator.
+%
+%                   - DIM1 and DIM2 denote the number of discrete CELLS,
+%                   - n1 and n2 denote the number of NODES inside the
+%                     domain .
+%                     especially NO boundary nodes! 
+%                   - h the meshwidth
+
+length_x = 1;
+length_y = 1;
+storage = 0;
+
+h_x = length_x/(N_x+1);
+h_y = length_y/(N_y+1);
+
+crosshairs_stencil = create_crosshairs_stencil(h_x,h_y);
+
+N = N_x * N_y;
+
+weights_matrix = zeros(N,N);
+
+index_displacement=[-N_x,-1,0,1,N_x];
+
+for i = 1:N_x
+    for j = 1:N_y
+        current_node_index=i+N_x*(j-1);
+        
+        %decide weather node is next to boundary node
+        x_up = i<N_x;
+        x_dwn = i>1;
+        y_up = j<N_y;
+        y_dwn = j>1;
+        
+        neighbors_indices = current_node_index + index_displacement(logical([y_dwn,x_dwn,1,x_up,y_up]));
+        
+        %assemble weights_matrix                                 
+        weights_matrix(current_node_index,neighbors_indices) = weights_matrix(current_node_index,neighbors_indices)...
+                            +crosshairs_stencil(logical([y_dwn,x_dwn,1,x_up,y_up]));
+    end
+end
+
+return
+
+
+
+
+
